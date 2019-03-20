44f2287aabe1c258e03c4e91ff72c46313603f84
Prokopchik Daria <dashaprokopchic@mail.ru>
2019-03-20 21:26:27 +0300

    add fapi

---
+++ fapi/src/main/java/com/netcracker/edu/name/fapi/controllers/SimpleController.java
@@ -0,0 +1,34 @@
+package com.netcracker.edu.name.fapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @RequestMapping(value = "/get-simple-object")
+    SimpleObject getSimpleString() {
+        SimpleObject simpleObject = new SimpleObject();
+        simpleObject.setId("1");
+        simpleObject.setName("name");
+        return simpleObject;
+    }
+}
+
+class SimpleObject {
+    String id;
+    String name;
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
